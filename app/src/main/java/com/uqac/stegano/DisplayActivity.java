2fe0102c4c0782061b542f44e6aaab6957e01ee8
Yaaaann <yroyant@enseirb-matmeca.fr>
2020-04-12 16:44:51 -0400

    made the link between encodeDecode and display image, app is working

--- app/src/main/java/com/uqac/stegano/DisplayActivity.java
+++ app/src/main/java/com/uqac/stegano/DisplayActivity.java
@@ -1,17 +1,22 @@
 package com.uqac.stegano;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.util.Util;
 
 
 public class DisplayActivity extends Activity {
+
+    private String photo_path = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,6 @@ public class DisplayActivity extends Activity {
         setContentView(R.layout.image_select);
 
         // Getting the image path
-        String photo_path = null;
         Bundle bundle = getIntent().getExtras();
         if (bundle != null)
             photo_path = bundle.getString("path");
@@ -31,11 +35,14 @@ public class DisplayActivity extends Activity {
             ImageView imageView = (ImageView) findViewById(R.id.singleImage);
             Bitmap myBitmap = Utilities.loadImage(photo_path);
             imageView.setImageBitmap(myBitmap);
+        } else {
+            Toast.makeText(getApplicationContext(), "Error loading image path", Toast.LENGTH_SHORT).show();
+            finish();
         }
 
         // Update the hidden text in the image
         TextView textView  = (TextView)  findViewById(R.id.imageText);
-        String decodeText = "Calling here the function to decode the image, or maybe we do it before and then test if there is anything, idk...";
+        String decodeText = EncodeDecode.decodeMessage(Utilities.loadImage(photo_path));
         textView.setText(decodeText);
 
         // Create the event for the button
@@ -43,7 +50,19 @@ public class DisplayActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String text = "Get the text in the editor";
+                EditText editText = (EditText) findViewById(R.id.userText);
+                String msg = editText.getText().toString();
+                if (!msg.matches("")) {
+                    Toast.makeText(getApplicationContext(), "Encryption started", Toast.LENGTH_SHORT).show();
+                    Bitmap bmpIn = Utilities.loadImage(photo_path);
+                    Bitmap bmpOut = EncodeDecode.encodeMessage(bmpIn, msg);
+                    String savePath = Utilities.saveToInternalStorage(bmpOut, "01.png"); // TODO : find a better file location, allow to save multiple images
+                    // Possibly restart this activity with the new image
+                    Toast.makeText(getApplicationContext(), "Saved in " + savePath, Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No message to encrypt", Toast.LENGTH_SHORT).show();
+                }
                 // Encode the text in image
             }
         });
