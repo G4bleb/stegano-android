6063e11219e680ec64613dc7cb20f2361a910532
Emtek <lagadecpa@gmail.com>
2020-04-14 11:22:27 -0400

    Added refresh and changed app icon

--- app/src/main/java/com/uqac/stegano/MainActivity.java
+++ app/src/main/java/com/uqac/stegano/MainActivity.java
@@ -2,6 +2,7 @@ package com.uqac.stegano;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -11,21 +12,21 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.text.Layout;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -38,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     private static final int PERMISSIONS_COUNT = 2;
+    private boolean isGalleryInitialized;
+    private SwipeRefreshLayout refreshLayout;
+
+    public Toast mToast;
 
     @SuppressLint("NewApi")
     private boolean arePermissionsDenied() {
@@ -64,7 +69,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isGalleryInitialized;
+    private void loadImages() {
+        final GalleryAdapter galleryAdapter = new GalleryAdapter();
+        final GridView gridView = findViewById(R.id.gridView);
+        final List<String> filesList = new ArrayList<>();
+        final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+        final String orderBy = MediaStore.Images.Media._ID;
+
+        //Stores all the images from the gallery in Cursor
+        Cursor cursor = getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+                null, orderBy);
+        //Total number of images
+        int count = cursor.getCount();
+
+        //Create an array to store path to all the images
+        String[] arrPath = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            cursor.moveToPosition(i);
+            int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+            //Store the path of the image
+            arrPath[i]= cursor.getString(dataColumnIndex);
+            //Log.i("PATH", arrPath[i]);
+        }
+        // The cursor should be freed up after use with close()
+        cursor.close();
+
+        for(int i=count-1 ; i >= 0 ; i--) { // reverse for latest image on top
+            if (arrPath[i].endsWith(".jpg") || arrPath[i].endsWith(".png") || arrPath[i].endsWith(".jpeg")) {
+                filesList.add(arrPath[i]);
+            }
+        }
+
+        galleryAdapter.setData(filesList);
+        gridView.setAdapter(galleryAdapter);
+
+        // Make grid clickable
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                String stringVal = filesList.get(pos);
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+                intent.putExtra("path", stringVal);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     protected void onResume() {
@@ -75,56 +126,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if(!isGalleryInitialized) {
-            final GridView gridView = findViewById(R.id.gridView);
-            final GalleryAdapter galleryAdapter = new GalleryAdapter();
-            final List<String> filesList = new ArrayList<>();
-
-            final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
-            final String orderBy = MediaStore.Images.Media._ID;
-            //Stores all the images from the gallery in Cursor
-            Cursor cursor = getContentResolver().query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
-                    null, orderBy);
-            //Total number of images
-            int count = cursor.getCount();
-
-            //Create an array to store path to all the images
-            String[] arrPath = new String[count];
-
-            for (int i = 0; i < count; i++) {
-                cursor.moveToPosition(i);
-                int dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
-                //Store the path of the image
-                arrPath[i]= cursor.getString(dataColumnIndex);
-                Log.i("PATH", arrPath[i]);
-            }
-            // The cursor should be freed up after use with close()
-            cursor.close();
-
-            for(int i=count-1 ; i >= 0 ; i--) { // reverse for latest image on top
-                if(arrPath[i].endsWith(".jpg") || arrPath[i].endsWith(".png") || arrPath[i].endsWith(".jpeg")) {
-                    filesList.add(arrPath[i]);
-                }
-            }
-
-            galleryAdapter.setData(filesList);
-            gridView.setAdapter(galleryAdapter);
+            loadImages();
             // isGalleryInitialized = true;
-
-            // Make grid clickable
-            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
-                    String stringVal = filesList.get(pos);
-                    Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
-                    intent.putExtra("path", stringVal);
-                    startActivity(intent);
-                }
-            });
         }
     }
-
-
 
     final class GalleryAdapter extends BaseAdapter {
 
@@ -171,7 +176,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         isGalleryInitialized = false;
+        mToast = Toast.makeText(getBaseContext(), "", Toast.LENGTH_SHORT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        refreshLayout = findViewById(R.id.swipeRefresh);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshImages();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menu_refresh) {
+            refreshImages();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refreshImages() {
+        mToast.setText("Refreshing");
+        mToast.show();
+        loadImages();
+        refreshLayout.setRefreshing(false);
+        mToast.setText("Refreshed");
+        mToast.show();
     }
 }
